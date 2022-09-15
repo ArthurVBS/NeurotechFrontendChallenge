2b759fcac7804559c24e3b10e9a8cd04a5115af6
ArthurVBS <arthurvini2703@gmail.com>
2022-09-14 22:38:36 -0300

    Change scripts in package.json

--- backend/src/config/dbConnection.js
+++ backend/src/config/dbConnection.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 module.exports = () => {
   try {
-    mongoose.connect(
-      process.env.MONGO_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-    );
+    mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    mongoose.connection.once('open', () => {
-      console.log('Connected to DB');
+    console.log("Connecting...");
+
+    mongoose.connection.once("open", () => {
+      console.log("Connected to DB");
     });
   } catch (error) {
     console.log(`Couldn't connect to DB ${error}`);
-  }  
+  }
 };
